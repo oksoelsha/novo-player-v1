c02982b1d0c15e5da7eabe8b1d34e3ca8f2ec79f
Sam Elsharif <oelsha@engineer.com>
2022-06-04 10:37:38 -0400

    fixed unit test

--- src/app/popups/bluemsx-arguments-edit/bluemsx-arguments-edit.component.spec.ts
+++ src/app/popups/bluemsx-arguments-edit/bluemsx-arguments-edit.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { BluemsxArgumentsEditComponent } from './bluemsx-arguments-edit.component';
 
@@ -8,6 +9,7 @@ describe('BluemsxArgumentsEditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ TranslateModule.forRoot() ],
       declarations: [ BluemsxArgumentsEditComponent ]
     })
     .compileComponents();
