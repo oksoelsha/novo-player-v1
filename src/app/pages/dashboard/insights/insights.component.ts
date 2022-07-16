74e4ad6d4d483905a83374ccb6696e31671ad4f2
Sam Elsharif <oelsha@engineer.com>
2022-07-15 20:52:29 -0400

    added graph for insight's total launches. Still need to provide real data

--- src/app/pages/dashboard/insights/insights.component.ts
+++ src/app/pages/dashboard/insights/insights.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexLegend, ApexNonAxisChartSeries, ApexStroke, ApexTooltip, ApexXAxis, ApexYAxis } from 'ng-apexcharts';
 import { EventsService } from '../../../services/events.service';
+import { LocalizationService } from '../../../services/localization.service';
 
 enum DisplayModes {topTen, launchTimes}
 
@@ -13,14 +15,80 @@ export class InsightsComponent implements OnInit {
   readonly pageSize = 5;
   topTenList: any[] = [];
   total = 0;
+
+  readonly chart: ApexChart = {
+    height: 180,
+    type: 'line',
+    toolbar: {
+      show: false
+    }
+  };
+
+  readonly stroke: ApexStroke = {
+    width: 2
+  };
+
+  readonly dataLabels: ApexDataLabels = {
+    enabled: false
+  };
+
+  readonly colors = ['#cccccc'];
+
+  readonly series: ApexAxisChartSeries = [
+    {
+      name: 'Total Launches',
+      data: [0, 0, 0, 0, 0, 0, 4, 10, 5, 19, 1, 7, 2, 2, 3, 0, 4, 10, 5, 12, 1, 7, 2, 2, 3, 0, 4, 10, 5, 12]
+    }
+  ];
+
+  readonly xaxis: ApexXAxis = {
+    categories: [],
+    labels: {
+      show: false
+    }
+  };
+
+  readonly yaxis: ApexYAxis = {
+    axisTicks: {
+      show: true
+    },
+    axisBorder: {
+      show: true,
+      color: '#cccccc'
+    },
+    labels: {
+      style: {
+        colors: '#cccccc'
+      }
+    },
+    title: {
+      text: 'Total Launches',
+      style: {
+        color: '#cccccc'
+      }
+    }
+  };
+
+  readonly tooltip: ApexTooltip = {
+    theme: 'dark'
+  };
+
   private cachedTopTenPageList: any[] = new Array(2);
   private currentDisplayMode: DisplayModes;
+  private readonly months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
-  constructor(private eventsService: EventsService) { }
+  constructor(private eventsService: EventsService, private localizationService: LocalizationService) { }
 
   ngOnInit(): void {
     this.currentDisplayMode = DisplayModes.topTen;
     this.getTopTenLaunchedGames(0);
+
+    // initialize the x-axis with the last 30 days in the format 'Mon day'
+    const now = Date.now();
+    for (let ix = 29; ix >= 0; ix--) {
+      const day = new Date(now - 86400000 * ix);
+      this.xaxis.categories.push(this.getMonth(day.getMonth()) + ' ' + day.getDate());
+    }
   }
 
   getTopTenLaunchedGames(page: number) {
@@ -49,5 +117,9 @@ export class InsightsComponent implements OnInit {
 
   setLaunchTimesMode() {
     this.currentDisplayMode = DisplayModes.launchTimes;
+  }
+
+  private getMonth(monthAsIndex: number): string {
+    return this.localizationService.translate('dashboard.' + this.months[monthAsIndex]);
   }
 }
