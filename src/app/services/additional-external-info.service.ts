5f33a065b20e1a82dd59f3ec771b9f3b66922d75
Sam Elsharif <oelsha@engineer.com>
2023-03-28 16:48:15 -0400

    updated Gian Bomb game name mapping

--- src/app/services/additional-external-info.service.ts
+++ src/app/services/additional-external-info.service.ts
@@ -62,19 +62,26 @@ export class AdditionalExternalInfoService {
   }
 
   private initIdToName() {
+    this.idToName.set(57, 'Hustle! Chumy');
+    this.idToName.set(204, 'Exerion II: Zorni');
     this.idToName.set(263, 'Hyper Sports');
     this.idToName.set(264, 'Hyper Sports II');
     this.idToName.set(272, 'MSX Baseball');
     this.idToName.set(318, 'HAL\'s Hole In One Golf');
+    this.idToName.set(319, 'Boggy \'84');
     this.idToName.set(378, 'Yie Ar Kung-Fu 2: The Emperor Yie-Gah');
     this.idToName.set(406, 'King\'s Valley');
     this.idToName.set(474, 'Ping Pong');
+    this.idToName.set(478, 'Golgo 13: Ōkami no Su');
     this.idToName.set(531, 'Kung-Fu Master');
     this.idToName.set(577, 'Hydlide');
     this.idToName.set(579, 'Hyper Sports III');
+    this.idToName.set(625, 'The Black Onyx');
+    this.idToName.set(674, 'Yokai Tantei Chima Chima');
     this.idToName.set(695, 'Genghis Khan');
     this.idToName.set(696, 'Vampire Killer');
     this.idToName.set(730, 'Come On Picot');
+    this.idToName.set(733, 'Gall Force: Defense of Chaos');
     this.idToName.set(739, 'Q*Bert');
     this.idToName.set(742, 'Gradius');
     this.idToName.set(754, 'Zanac');
@@ -82,10 +89,12 @@ export class AdditionalExternalInfoService {
     this.idToName.set(785, 'Dunkshot');
     this.idToName.set(825, 'Hydlide II: Shine of Darkness');
     this.idToName.set(830, 'MSX Baseball II');
+    this.idToName.set(841, 'The Black Onyx II: Search For The Fire Crystal');
     this.idToName.set(855, 'Knightmare');
     this.idToName.set(860, 'Valis: The Fantasm Soldier');
     this.idToName.set(873, 'Penguin Adventure');
     this.idToName.set(880, 'Romancia: Dragon Slayer Jr.');
+    this.idToName.set(894, 'Wingman 2: Kitakura no Fukkatsu');
     this.idToName.set(898, 'Ultima IV: Quest of the Avatar');
     this.idToName.set(905, 'F-1 Spirit: The Way To Formula-1');
     this.idToName.set(916, 'Knightmare II: The Maze of Galious');
@@ -101,6 +110,7 @@ export class AdditionalExternalInfoService {
     this.idToName.set(1073, 'F-1 Spirit: 3D Special');
     this.idToName.set(1078, 'King\'s Valley 2');
     this.idToName.set(1079, 'King\'s Valley 2');
+    this.idToName.set(1117, 'Cosmic Soldier 2: Psychic War');
     this.idToName.set(1119, 'Psychic World');
     this.idToName.set(1188, 'Parodius');
     this.idToName.set(1239, 'Ys III: Wanderers from Ys');
@@ -119,10 +129,13 @@ export class AdditionalExternalInfoService {
     this.idToName.set(1666, 'Action RPG Construction Tool: Dante 2');
     this.idToName.set(1703, 'Dead of the Brain: Shiryou no Sakebi');
     this.idToName.set(1707, 'The Tower? Of Cabin: Cabin Panic');
+    this.idToName.set(1798, 'BC\'s Quest for Tires');
     this.idToName.set(1843, 'Jack Nicklaus\' Greatest 18 Holes of Major Championship Golf');
     this.idToName.set(2414, 'Who Dares Wins II');
+    this.idToName.set(2601, 'BC\'s Quest for Tires II: Grog\'s Revenge');
     this.idToName.set(2911, 'The Activision Decathlon');
     this.idToName.set(3278, 'The Day II');
     this.idToName.set(3280, 'Agi Gongnyong Dooly');
+    this.idToName.set(3687, 'Universe: Unknown');
   }
 }
