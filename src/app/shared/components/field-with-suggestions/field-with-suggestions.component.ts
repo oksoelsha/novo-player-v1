f58eeb0e952ef276439d9d4d62d540ef3679a6fa
Sam Elsharif <oelsha@engineer.com>
2023-07-27 15:31:05 -0400

    improved suggestions by filtering list based on input

--- src/app/shared/components/field-with-suggestions/field-with-suggestions.component.ts
+++ src/app/shared/components/field-with-suggestions/field-with-suggestions.component.ts
@@ -18,6 +18,7 @@ export class FieldWithSuggestionsComponent {
   @ViewChildren('suggestionsList') private suggestionsList: QueryList<ElementRef>;
 
   suggestions: string[] = [];
+  private allPossibleSuggestions: string[] = [];
 
   constructor() { }
 
@@ -33,15 +34,19 @@ export class FieldWithSuggestionsComponent {
 
   onSelectSuggestion(event: any, suggestion: string) {
     if (this.suggestionsDropdown.isOpen()) {
+      const lastIndexTrigger = this.value.lastIndexOf(this.trigger);
+      const lastIndexSpace = this.value.lastIndexOf(' ');
+      const lastIndexTriggerOrSpace = this.getLastIndexTriggerOrSpace(lastIndexTrigger, lastIndexSpace);
+      const valueToAppendTo = this.value.substring(0, lastIndexTriggerOrSpace + 1);
+
       if (suggestion.indexOf(' ') > -1) {
-        this.value = this.value + '"' + suggestion + '"';
+        this.value = valueToAppendTo + '"' + suggestion + '"';
       } else {
-        this.value = this.value + suggestion;
+        this.value = valueToAppendTo + suggestion;
       }
       this.inputField.nativeElement.focus();
-      setTimeout(() => {
-        this.showSuggestionsForGivenInput(this.value);
-      }, 0);
+      this.suggestionsDropdown.close();
+      this.allPossibleSuggestions = [];
       this.userInputOutput.emit(this.value);
     }
     event.preventDefault();
@@ -50,28 +55,47 @@ export class FieldWithSuggestionsComponent {
 
   private showSuggestionsForGivenInput(inputText: string) {
     if (inputText) {
-      if (inputText.lastIndexOf(this.trigger) === inputText.length - 1) {
-        this.suggestions = Array.from( this.suggestionsMap.keys() );
-        this.suggestionsDropdown.open();
-      } else {
-        const lastIndexTrigger = inputText.lastIndexOf(this.trigger);
-        const lastIndexSpace = inputText.lastIndexOf(' ');
-        if (lastIndexTrigger > -1 && lastIndexSpace === inputText.length - 1) {
-          const argument = inputText.substring(lastIndexTrigger + 1, lastIndexSpace);
-          const values = this.suggestionsMap.get(argument);
-          if (values != null) {
-            this.suggestions = values;
-            this.suggestionsDropdown.open();
-          } else {
-            this.suggestionsDropdown.close();
-          }
+      const lastIndexTrigger = inputText.lastIndexOf(this.trigger);
+      const lastIndexSpace = inputText.lastIndexOf(' ');
+      if (lastIndexTrigger === inputText.length - 1) {
+        this.allPossibleSuggestions = Array.from(this.suggestionsMap.keys());
+        this.setSuggestionsList(inputText, lastIndexTrigger, -1);
+      } else if (lastIndexTrigger > -1 && lastIndexSpace === inputText.length - 1) {
+        const argument = inputText.substring(lastIndexTrigger + 1, lastIndexSpace);
+        const values = this.suggestionsMap.get(argument);
+        if (values != null) {
+          this.allPossibleSuggestions = values;
+          this.setSuggestionsList(inputText, lastIndexTrigger, lastIndexSpace);
         } else {
           this.suggestionsDropdown.close();
         }
+      } else {
+        this.setSuggestionsList(inputText, lastIndexTrigger, lastIndexSpace);
       }
     } else {
       this.suggestionsDropdown.close();
     }
     this.userInputOutput.emit(this.value);
+  }
+
+  private setSuggestionsList(inputText: string, lastIndexTrigger: number, lastIndexSpace: number) {
+    const lastIndexTriggerOrSpace = this.getLastIndexTriggerOrSpace(lastIndexTrigger, lastIndexSpace);
+
+    if (inputText.length - 1 > lastIndexTriggerOrSpace) {
+      const inputTextPortion = inputText.substring(lastIndexTriggerOrSpace + 1);
+      this.suggestions = this.allPossibleSuggestions.filter(s => s.startsWith(inputTextPortion));
+    } else {
+      this.suggestions = this.allPossibleSuggestions;
+    }
+
+    this.suggestionsDropdown.open();
+  }
+
+  private getLastIndexTriggerOrSpace(lastIndexTrigger: number, lastIndexSpace: number): number {
+    if (lastIndexTrigger > lastIndexSpace) {
+      return lastIndexTrigger;
+    } else {
+      return lastIndexSpace;
+    }
   }
 }
