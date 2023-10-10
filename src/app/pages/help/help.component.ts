572b5ced686e5a598dc0a5547ba7318735f8107b
Sam Elsharif <oelsha@engineer.com>
2023-10-09 20:19:51 -0400

    updated for v1.6

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -8,7 +8,7 @@ import { VersionsService } from '../../services/versions.service';
 })
 export class HelpComponent implements OnInit {
 
-  readonly applicationVersion = '1.5.2';
+  readonly applicationVersion = '1.6';
 
   applicationVersions: Promise<any>;
   screenshotsVersions: Promise<any>;
