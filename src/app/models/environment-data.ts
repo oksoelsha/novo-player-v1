6c9a57ab34938e3bd6ce0dc15ac215831d2c37f7
Sam Elsharif <oelsha@engineer.com>
2023-12-05 15:09:24 -0500

    fix backwards support (going back to older instance should work after using the newer one)

--- src/app/models/environment-data.ts
+++ src/app/models/environment-data.ts
@@ -1,7 +1,9 @@
 export class EnvironmentData {
     readonly applicationVersion: string;
+    readonly extraDataVersion: string;  // needed for backwards compatibility
 
     constructor(applicationVersion: string) {
         this.applicationVersion = applicationVersion;
+        this.extraDataVersion = '';
     }
 }
