f3644c32bdbf6ff4b616138244ea0caaeeaa493a
Sam Elsharif <oelsha@engineer.com>
2024-01-30 08:03:47 -0500

    guard against a null pointer (that I don't know why it happened)

--- app/OpenMSXConnectionManager.ts
+++ app/OpenMSXConnectionManager.ts
@@ -77,7 +77,7 @@ export class OpenMSXConnectionManager {
                 const type = result.update['@_type'];
                 if (type === 'setting') {
                     const name = result.update['@_name'];
-                    const state = result.update['#text'].toString();
+                    const state = result.update['#text']?.toString();
                     this.updateEmitter.emit('openmsxUpdate', openmsxConnector.pid, type, name, state);
                 }
             }
