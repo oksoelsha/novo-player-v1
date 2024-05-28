0ada58a7d91d68038ea2e585bd9ca6abffad97a1
Sam Elsharif <oelsha@engineer.com>
2024-05-27 21:07:34 -0400

    fixed test

--- src/app/pages/dashboard/pipes/long-event-date.pipe.spec.ts
+++ src/app/pages/dashboard/pipes/long-event-date.pipe.spec.ts
@@ -10,7 +10,7 @@ describe('LongEventDatePipe', () => {
 describe('LongEventDatePipe', () => {
   it('transform function should return mm-dd for given date number', () => {
     const pipe = new LongEventDatePipe();
-    expect(pipe.transform(1698847666000)).toEqual('11/1/2023, 10:07:46 AM');
-    expect(pipe.transform(1696038322000)).toEqual('9/29/2023, 9:45:22 PM');
+    expect(pipe.transform(1698847666000)).toEqual(new Date(1698847666000).toLocaleString());
+    expect(pipe.transform(1696038322000)).toEqual(new Date(1696038322000).toLocaleString());
   });
 });
