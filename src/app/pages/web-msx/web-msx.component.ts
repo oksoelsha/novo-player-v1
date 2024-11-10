86fa317295e1ee657837e1ed9bc68d8cc377eb0f
Sam Elsharif <oelsha@engineer.com>
2024-11-10 18:14:48 -0500

    first phase of passwords for WebMSX

--- src/app/pages/web-msx/web-msx.component.ts
+++ src/app/pages/web-msx/web-msx.component.ts
@@ -1,24 +1,32 @@
-import { Component, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Game } from '../../models/game';
 import { Settings } from '../../models/settings';
 import { EmulatorService } from '../../services/emulator.service';
 import { SettingsService } from '../../services/settings.service';
 import { WindowService } from '../../services/window.service';
+import { GamePassword, GamePasswordsInfo } from '../../models/game-passwords-info';
+import { GamesService } from '../../services/games.service';
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-web-msx',
   templateUrl: './web-msx.component.html',
-  styleUrls: ['./web-msx.component.sass']
+  styleUrls: ['../../common-styles.sass', './web-msx.component.sass']
 })
 export class WebMSXComponent implements OnInit, OnDestroy {
 
+  @ViewChildren('passwordsDropdown') private passwordsDropdown: QueryList<NgbDropdown>;
   selectedGame: Game;
   error: boolean;
+  gamePasswordsInfo: GamePasswordsInfo;
+  selectedPassword: GamePassword;
   private wmsxScript: any;
+  private msxKeyCodeByCharacter = new Map<string, number>();
 
   constructor(private renderer: Renderer2, private route: ActivatedRoute, private settingsService: SettingsService,
-    private emulatorService: EmulatorService, private router: Router, private windowService: WindowService) {
+    private emulatorService: EmulatorService, private router: Router, private windowService: WindowService,
+    private gamesService: GamesService) {
     this.selectedGame = JSON.parse(route.snapshot.paramMap.get('gameParams'));
   }
 
@@ -39,6 +47,8 @@ export class WebMSXComponent implements OnInit, OnDestroy {
         }
       });
     });
+    this.setPasswords();
+    this.initializeKeysMap();
   }
 
   loadWebMSX(fullpath: string) {
@@ -65,5 +75,83 @@ export class WebMSXComponent implements OnInit, OnDestroy {
       window['WMSX'].shutdown();
       this.renderer.removeChild(document.body, this.wmsxScript);
     }
+  }
+
+  async enterPassword() {
+//    window['WMSX'].room.keyboard.typeString('A');
+    const passwordsDropdownArray = this.passwordsDropdown.toArray();
+    if (passwordsDropdownArray.length === 1) {
+      passwordsDropdownArray[0].close();
+    }
+    const password = this.selectedPassword.password;
+    for (let i = 0; i < password.length; i++) {
+      if (password[i] === ' ') {
+        await this.pressKey(this.msxKeyCodeByCharacter.get('Space'));
+      } else {
+        await this.pressKey(this.msxKeyCodeByCharacter.get(password[i]));
+      }
+    }
+    if (this.selectedPassword.pressReturn) {
+      await this.pressKey(this.msxKeyCodeByCharacter.get('Enter'));
+    }
+  }
+
+  private initializeKeysMap() {
+    this.msxKeyCodeByCharacter.set('1', 1);
+    this.msxKeyCodeByCharacter.set('2', 2);
+    this.msxKeyCodeByCharacter.set('3', 3);
+    this.msxKeyCodeByCharacter.set('4', 4);
+    this.msxKeyCodeByCharacter.set('5', 5);
+    this.msxKeyCodeByCharacter.set('6', 6);
+    this.msxKeyCodeByCharacter.set('7', 7);
+    this.msxKeyCodeByCharacter.set('8', 8);
+    this.msxKeyCodeByCharacter.set('9', 9);
+    this.msxKeyCodeByCharacter.set('0', 10);
+    this.msxKeyCodeByCharacter.set('Q', 101);
+    this.msxKeyCodeByCharacter.set('W', 102);
+    this.msxKeyCodeByCharacter.set('E', 103);
+    this.msxKeyCodeByCharacter.set('R', 104);
+    this.msxKeyCodeByCharacter.set('T', 105);
+    this.msxKeyCodeByCharacter.set('Y', 106);
+    this.msxKeyCodeByCharacter.set('U', 107);
+    this.msxKeyCodeByCharacter.set('I', 108);
+    this.msxKeyCodeByCharacter.set('O', 109);
+    this.msxKeyCodeByCharacter.set('P', 110);
+    this.msxKeyCodeByCharacter.set('A', 111);
+    this.msxKeyCodeByCharacter.set('S', 112);
+    this.msxKeyCodeByCharacter.set('D', 113);
+    this.msxKeyCodeByCharacter.set('F', 114);
+    this.msxKeyCodeByCharacter.set('G', 115);
+    this.msxKeyCodeByCharacter.set('H', 116);
+    this.msxKeyCodeByCharacter.set('J', 117);
+    this.msxKeyCodeByCharacter.set('K', 118);
+    this.msxKeyCodeByCharacter.set('L', 119);
+    this.msxKeyCodeByCharacter.set('Z', 120);
+    this.msxKeyCodeByCharacter.set('X', 121);
+    this.msxKeyCodeByCharacter.set('C', 122);
+    this.msxKeyCodeByCharacter.set('V', 123);
+    this.msxKeyCodeByCharacter.set('B', 124);
+    this.msxKeyCodeByCharacter.set('N', 125);
+    this.msxKeyCodeByCharacter.set('M', 126);
+    this.msxKeyCodeByCharacter.set('Enter', 204);
+    this.msxKeyCodeByCharacter.set('Space', 205);
+    this.msxKeyCodeByCharacter.set('-', 222);
+  }
+
+  private async pressKey(keyCode: number) {
+    window['WMSX'].room.keyboard.processKey(keyCode, 1);
+    await this.delay();
+    window['WMSX'].room.keyboard.processKey(keyCode, 0);
+    await this.delay();  
+  }
+
+  private async delay() {
+    return new Promise(resolve => setTimeout(resolve, 80));
+  };
+
+  private setPasswords() {
+    this.gamesService.getGamePasswords(this.selectedGame).then((gamePasswordsInfo) => {
+      this.gamePasswordsInfo = gamePasswordsInfo;
+    });
   }
 }
