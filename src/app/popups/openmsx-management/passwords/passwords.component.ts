6c232885ad821d4cc3729b6b706fadc933859248
Sam Elsharif <oelsha@engineer.com>
2024-11-12 11:27:34 -0500

    fixed minor things around game passwords

--- src/app/popups/openmsx-management/passwords/passwords.component.ts
+++ src/app/popups/openmsx-management/passwords/passwords.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { GamePassword, GamePasswordsInfo } from '../../../models/game-passwords-info';
 import { LaunchActivityService } from '../../../services/launch-activity.service';
 import { LocalizationService } from '../../../services/localization.service';
@@ -16,7 +16,22 @@ export class PasswordsComponent {
   @Input() gamePasswordsInfo: GamePasswordsInfo;
   @Output() alertMessage: EventEmitter<string> = new EventEmitter<string>();
 
+  clearEventSubject: Subject<void> = new Subject<void>();
+  private eventsSubscription: Subscription;
+
   constructor(private launchActivityService: LaunchActivityService, private localizationService: LocalizationService) { }
+
+  ngOnInit(): void {
+    this.eventsSubscription = this.events.subscribe((flag) => {
+      if (!flag) {
+        this.clearEventSubject.next();
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.eventsSubscription?.unsubscribe();
+  }
 
   enter(selectedPassword: GamePassword) {
     this.launchActivityService.enterPassword(this.pid, selectedPassword).then(entered => {
--- src/app/shared/components/password-selector/password-selector.component.ts
+++ src/app/shared/components/password-selector/password-selector.component.ts
@@ -1,20 +1,34 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { GamePassword, GamePasswordsInfo } from '../../../models/game-passwords-info';
 import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-password-selector',
   templateUrl: './password-selector.component.html',
   styleUrls: ['../../../common-styles.sass', './password-selector.component.sass']
 })
-export class PasswordSelectorComponent {
+export class PasswordSelectorComponent implements OnInit, OnDestroy {
 
   @Input() gamePasswordsInfo: GamePasswordsInfo;
+  @Input() clearEvent: Observable<void>;
   selectedPassword: GamePassword;
   @Output() passwordToEnter: EventEmitter<GamePassword> = new EventEmitter<GamePassword>();
   @ViewChild('passwordsDropdown', { static: true }) dropdown: NgbDropdown;
 
+  private clearEventSubscription: Subscription;
+
   constructor() { }
+  ngOnInit(): void {
+    this.selectedPassword = null;
+    this.clearEventSubscription = this.clearEvent?.subscribe(() => {
+      this.selectedPassword = null;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.clearEventSubscription?.unsubscribe();
+  }
 
   enter() {
     this.passwordToEnter.emit(this.selectedPassword);
