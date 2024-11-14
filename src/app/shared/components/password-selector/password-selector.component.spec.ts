2df27b9c950431d1229b3923ce4285c0cb378ac1
Sam Elsharif <oelsha@engineer.com>
2024-11-13 22:08:32 -0500

    fixed unit test

--- src/app/shared/components/password-selector/password-selector.component.spec.ts
+++ src/app/shared/components/password-selector/password-selector.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordSelectorComponent } from './password-selector.component';
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateModule } from '@ngx-translate/core';
+import { GamePasswordsInfo } from '../../../models/game-passwords-info';
 
 describe('PasswordSelectorComponent', () => {
   let component: PasswordSelectorComponent;
@@ -8,7 +11,11 @@ describe('PasswordSelectorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PasswordSelectorComponent ]
+      imports: [ TranslateModule.forRoot() ],
+      declarations: [
+        PasswordSelectorComponent,
+        NgbDropdown
+      ]
     })
     .compileComponents();
   });
@@ -16,6 +23,7 @@ describe('PasswordSelectorComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PasswordSelectorComponent);
     component = fixture.componentInstance;
+    component.gamePasswordsInfo = new GamePasswordsInfo([], 'setup');
     fixture.detectChanges();
   });
 
